8a575fc42144d7007d4167df699345c4f5015b21
Xananax <xananax@yelostudio.com>
2018-04-16 16:34:29 +0300

    added not found route

--- src/components/App.js
+++ src/components/App.js
@@ -4,9 +4,12 @@ import Switch from 'react-router-dom/Switch';
 import Home from './Home';
 import './App.css';
 
+const NotFound = () => <div>...no</div>
+
 const App = () => (
   <Switch>
     <Route exact path="/" component={Home} />
+    <Route component={NotFound} />
   </Switch>
 );
 
