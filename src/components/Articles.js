e9466acb8fac063595071c2886af9b8e8d1a29d5
fadi <fadi mabsaleh>
2018-05-14 11:32:03 +0300

    changed Articles to class Comp.

--- src/components/Articles.js
+++ src/components/Articles.js
@@ -10,28 +10,29 @@ fetch('/quotes')
   console.log(data) 
 })
 
+export class Articles extends React.Component{
+  render(){
+    let quoteID;
+    
+    // check if we have number
+    if (props.match.params.quoteID) {
+      quoteID = props.match.params.quoteID
+    } else {
+      quoteID = 0
+    }
+    
+    quoteID = parseInt(quoteID, 10)
+    
+    // check if we have quote with this number 
+    if (isNaN(quoteID) || quoteID < 0 || quoteID > quotesList.length) {
+      return <div>this is not a valid page number</div>
+    }
+    const quote = quotesList[quoteID]
+    console.log(quote)
+    
+    return <ArticlesPage quote={quote} pageNumber={quoteID} />
 
-export const Articles = (props) => {
-
-  let quoteID;
-
-  // check if we have number
-  if (props.match.params.quoteID) {
-    quoteID = props.match.params.quoteID
-  } else {
-    quoteID = 0
   }
-
-  quoteID = parseInt(quoteID, 10)
-
-  // check if we have quote with this number 
-  if (isNaN(quoteID) || quoteID < 0 || quoteID > quotesList.length) {
-    return <div>this is not a valid page number</div>
-  }
-  const quote = quotesList[quoteID]
-  console.log(quote)
-
-  return <ArticlesPage quote={quote} pageNumber={quoteID} />
 }
 
 // stop defaul submit for the form
