94faae9de8b7e67296ff3ce49b9704b8254ae4a9
fadi <fadi mabsaleh>
2018-05-14 10:59:25 +0300

    import testing data, edit for render it

--- src/components/Articles.js
+++ src/components/Articles.js
@@ -1,28 +1,35 @@
 import React from 'react'
 import { PageTemplate } from './PageTemplate'
+import { quotesList } from './data'
 
 export const Articles = (props) => {
-  
-  let x;
-  if(props.match.params.pageNumber){
-    x = props.match.params.pageNumber
-  }else{
-    x = 0
+
+  let quoteID;
+
+  // check if we have number
+  if (props.match.params.quoteID) {
+   quoteID= props.match.params.quoteID
+  } else {
+   quoteID= 0
   }
-  
-  x = parseInt(x,10)
-  
-  if(isNaN(x)){
+
+ quoteID= parseInt(quoteID, 10)
+
+  // check if we have quote with this number 
+  if (isNaN(quoteID) ||quoteID< 0 ||quoteID> quotesList.length) {
     return <div>this is not a valid page number</div>
   }
-  
-  return <ArticlesPage pageNumber={x}/>
+  const quote = quotesList[quoteID]
+  console.log(quote)
+
+  return <ArticlesPage quote={quote} pageNumber={quoteID} />
 }
 
 export const ArticlesPage = (props) => (
   <PageTemplate header="Articles">
     <div>
-      the articles page {props.pageNumber}
+      <h4>{props.quote.author}</h4>
+      <p>{props.quote.quote}</p>
     </div>
   </PageTemplate>
 )
