58e5bc63e5b778e77974c9530147944cc9765bf7
fadi <fadi mabsaleh>
2018-05-14 11:16:09 +0300

    add form and submit function for get new quote

--- src/components/Articles.js
+++ src/components/Articles.js
@@ -8,15 +8,15 @@ export const Articles = (props) => {
 
   // check if we have number
   if (props.match.params.quoteID) {
-   quoteID= props.match.params.quoteID
+    quoteID = props.match.params.quoteID
   } else {
-   quoteID= 0
+    quoteID = 0
   }
 
- quoteID= parseInt(quoteID, 10)
+  quoteID = parseInt(quoteID, 10)
 
   // check if we have quote with this number 
-  if (isNaN(quoteID) ||quoteID< 0 ||quoteID> quotesList.length) {
+  if (isNaN(quoteID) || quoteID < 0 || quoteID > quotesList.length) {
     return <div>this is not a valid page number</div>
   }
   const quote = quotesList[quoteID]
@@ -25,11 +25,28 @@ export const Articles = (props) => {
   return <ArticlesPage quote={quote} pageNumber={quoteID} />
 }
 
+// stop defaul submit for the form
+// get the values from the form 
+const onSubmit = (evt) => {
+  evt.preventDefault()
+  const form = evt.form
+  const quote = form.quote.value
+  const author = form.author.value
+  const obj = { quote, author }
+}
+
 export const ArticlesPage = (props) => (
   <PageTemplate header="Articles">
     <div>
       <h4>{props.quote.author}</h4>
       <p>{props.quote.quote}</p>
+      <form onSubmit={this.onSubmit} >
+        <input type='text' placeholder='quote' />
+        <br />
+        <input type='text' placeholder='author' />
+        <br />
+        <input type='submit' value='submit' />
+      </form>
     </div>
   </PageTemplate>
 )
